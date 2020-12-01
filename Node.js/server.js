22bdfa49b37e6d9f404bdb6776208671708bb71a
aryap12 <70554757+aryap12@users.noreply.github.com>
2020-12-01 20:24:31 +0700

    hore kelar dikit

--- Node.js/server.js
+++ Node.js/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("views"));
 
 app.get('/',function(req,res){
     res.render('pages/index');
