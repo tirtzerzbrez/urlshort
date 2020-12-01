f3a8cdf5c6994299791a82db14a13b0f2f3c67a5
VincentFernandes <fernandesvincent62@gmail.com>
2020-12-01 20:16:06 +0700

    gambar

--- Node.js/server.js
+++ Node.js/server.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var bodyParser= require('body-parser')
+var bodyParser= require('body-parser');
 var app = express();
 
 app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use( express.static( "views" ) );
 
 app.get('/',function(req,res){
     res.render('pages/index');
